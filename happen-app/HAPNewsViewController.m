82324d28d61c928903e941b9a09b77a9eddb3e14
Sam Couch <sccouch@gmail.com>
2014-02-26 12:59:41 -0800

    added multipleViewController to News tab

---
+++ happen-app/HAPNewsViewController.m
@@ -0,0 +1,23 @@
+//
+//  HAPNewsViewController.m
+//  happen-app
+//
+//  Created by Sam Couch on 2/26/14.
+//  Copyright (c) 2014 Happen. All rights reserved.
+//
+
+#import "HAPNewsViewController.h"
+
+@implementation HAPNewsViewController
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    NSMutableArray *initialViewController = [NSMutableArray array];
+    [initialViewController addObject:[self.storyboard instantiateViewControllerWithIdentifier:@"NotificationsView"]];
+    [initialViewController addObject:[self.storyboard instantiateViewControllerWithIdentifier:@"FriendRequestView"]];
+    
+    self.viewController = initialViewController;
+}
+
+@end
